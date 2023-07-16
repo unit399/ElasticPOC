35f53e3febaaa5692a4ecfb4a41f11b7f1007815
Okan Çilingiroğlu <okan.cilingiroglu@gmail.com>
2023-07-16 16:56:37 +0300

    first commit

---
+++ ElasticPOC/Extensions/ElasticSearchExtensions.cs
@@ -0,0 +1,38 @@
+﻿using Nest;
+using Notification.Application.Models;
+
+namespace Notification.Application.Extensions;
+
+public static class ElasticSearchExtensions
+{
+    public static void AddElasticSearch(this IServiceCollection services, IConfiguration configuration)
+    {
+        var url = configuration["ELKConfiguration:Uri"];
+        var defaultIndex = configuration["ELKConfiguration:DefaultIndex"];
+
+        var settings = new ConnectionSettings(new Uri(url)).PrettyJson().DefaultIndex(defaultIndex);
+
+        AddDefaultMappings(settings);
+
+        var client = new ElasticClient(settings);
+        services.AddSingleton<IElasticClient>(client);
+
+        CreateIndex(client, defaultIndex);
+    }
+
+    private static void AddDefaultMappings(ConnectionSettings settings)
+    {
+        settings.DefaultMappingFor<Product>(p =>
+            p.Ignore(x => x.Price)
+                .Ignore(x => x.Id)
+                .Ignore(x => x.Quantity));
+    }
+
+    private static void CreateIndex(IElasticClient client, string indexName)
+    {
+        if (string.IsNullOrWhiteSpace(indexName))
+            return;
+
+        client.Indices.Create(indexName, i => i.Map<Product>(m => m.AutoMap()));
+    }
+}
---
+++ ElasticPOC/Program.cs
@@ -0,0 +1,33 @@
+﻿using Notification.Application.Extensions;
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Host.UseSerilog((context, configuration) =>
+    configuration.ReadFrom.Configuration(context.Configuration));
+
+builder.Services.AddElasticSearch(builder.Configuration);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseSerilogRequestLogging();
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
